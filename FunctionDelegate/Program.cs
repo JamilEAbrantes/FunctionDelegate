aca3eb42cea3aa53b76d6c719822955250d87d56
Jamil Abrantes <jamilabrantes.dev@outlook.com>
2019-02-05 18:04:30 -0200

    Acrescentado novas funções.

--- FunctionDelegate/Program.cs
+++ FunctionDelegate/Program.cs
@@ -5,27 +5,38 @@ using System;
 namespace FunctionDelegate
 {
     public class Program
-    {
+    {      
         static void Main(string[] args)
         {
+            var valor1 = obterNumeroAleatorio.Invoke();
+            var valor2 = obterNumeroAleatorio.Invoke();
+
             var stopWatch = new Stopwatch();
             stopWatch.Start();
 
-            LogCalculo(ExecutarSoma, new Somar(10, 10));
-            LogCalculo(ExecutarSubtracao, new Subtrair(12, 10));
-            LogCalculo(ExecutarMultiplicacao, new Multiplicar(10, 100));
-            LogCalculo(ExecutarDivisao, new Dividir(10, 2));
+            LogCalculo(ExecutarSoma, new Somar(valor1, valor2));
+            LogCalculo(ExecutarSubtracao, new Subtrair(valor1, valor2));
+            LogCalculo(ExecutarMultiplicacao, new Multiplicar(valor1, valor2));
+            LogCalculo(ExecutarDivisao, new Dividir(valor1, valor2));
 
             stopWatch.Stop();
-            Console.WriteLine($"Tempo de execução do método { stopWatch.ElapsedMilliseconds } milisegundos.");
+            escrever($"Tempo de execução do método { stopWatch.ElapsedMilliseconds } milisegundos.");
 
             Console.ReadKey();
         }
 
         static void LogCalculo<TObjeto, TRetorno>(Func<TObjeto, TRetorno> metodoSolicitado, TObjeto objetoSolicitado)
         {
-            Console.WriteLine($"Desc. -> { typeof(TObjeto).Name }, saída -> { JsonConvert.SerializeObject(metodoSolicitado(objetoSolicitado)) };");
+            escrever($"Desc. -> { typeof(TObjeto).Name }, saída -> { JsonConvert.SerializeObject(metodoSolicitado(objetoSolicitado)) };");
         }
+
+        static Action<string> escrever = str => Console.WriteLine(str);
+
+        static Func<int> obterNumeroAleatorio = () =>
+        {
+            var rnd = new Random();
+            return rnd.Next(0, 100);
+        };
 
         #region --> Métodos
 
@@ -96,7 +107,21 @@ namespace FunctionDelegate
             this.V1 = v1;
             this.V2 = v2;
         }
-        public override void AtribuirResultado() => this.Resultado = this.V1 / this.V2;
+        public override void AtribuirResultado()
+        {
+            AtribuirValorSeDivisorEhZero((x) => x == 0);
+            this.Resultado = this.V1 / this.V2;
+        } 
+        public void AtribuirValorSeDivisorEhZero(Predicate<double> predicate)
+        {
+            if (predicate(this.V2))
+                this.V2 = Divisor.Minimo;
+        }
+    }
+    public struct Divisor
+    {
+        public const int Minimo = 1;
+        public const int Maximo = 10;
     }
 
     #endregion
